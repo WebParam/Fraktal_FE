4e2cb3057778d0930b648443ea497eba4e7658ef
bradleymatjie <bradleysaint45@gmail.com>
2023-09-19 16:33:01 +0200

    first commit

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+'use client'
+import './Header.scss';
+
+
+function Header() {
+    return (
+        <header className='header'>
+         header
+        </header>
+    )
+}
+
+export default Header;
