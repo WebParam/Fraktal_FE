a2c9d3b1c706afea6601aadd4f75dc3d3b1c6ad8
bradleymatjie <bradleysaint45@gmail.com>
2023-09-20 21:58:24 +0200

    testimony section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Header from './components/Header/Header'
 import MobileMenu from './components/MobileMenu/MobileMenu'
 import Hero from './components/Home/Hero';
 import SecondSection from './components/SecondSection/SecondSection';
+import Testimony from './components/Testimony/Testimony';
 
 export default function Home() {
   const [menuToggler, setMenuToggler] = useState<boolean>(false);
@@ -24,6 +25,7 @@ export default function Home() {
       <MobileMenu removeMenu={removeMenu} menuToggler={menuToggler} />
       <Hero />
       <SecondSection />
+      <Testimony />
     </>
   )
 }
