23ddfbdc14ef63c318bebe06ccdcd1faf560cd5b
Bradley Matjie <bradley@webparam.org>
2023-10-10 15:34:39 +0200

    stable version

---
+++ src/app/TypedText.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import Typed from 'typed.js';
+
+const TypedText: React.FC = () => {
+  let typedInstance: Typed | undefined;
+
+  useEffect(() => {
+    const options = {
+      strings: ["Developer", "Gig."],
+      typeSpeed: 90,
+      loop: true,
+      backSpeed: 90,
+      backDelay: 2500,
+    };
+
+    const typedElement = document.querySelector('.js-typedjs');
+    if (typedElement) {
+      typedInstance = new Typed(typedElement, options);
+    }
+
+    // Cleanup on component unmount
+    return () => {
+      if (typedInstance) {
+        typedInstance.destroy();
+      }
+    };
+  }, []);
+
+  return <span className="js-typedjs">Developer</span>;
+};
+
+export default TypedText;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image';
+import './homepage.scss';
 import heroImage from '../assets/additional/heroImage.jpg';
 import card1 from "../assets/img/480x220/img1.jpg";
 import card2 from "../assets/img/480x220/img2.jpg";
@@ -15,6 +16,8 @@ import smallAvatar from '../assets/additional/testimonyAvatar.jpg';
 import card4 from "../assets/img/480x320/img33.jpg";
 import card5 from "../assets/img/480x320/img34.jpg";
 import card6 from "../assets/img/480x320/img35.jpg";
+import React, { useEffect } from 'react';
+import TypedText from './TypedText';
 
 export default function HomePage() {
 
@@ -33,13 +36,9 @@ export default function HomePage() {
                   <h1 className="display-4 mb-3">
                     Looking for a Fraktional
                     <span className="text-primary text-highlight-warning">
-                      <span className="js-typedjs" data-hs-typed-options="{
-                        &quot;strings&quot;: [&quot;Developer.&quot;, &quot;Gig.&quot;],
-                        &quot;typeSpeed&quot;: 90,
-                        &quot;loop&quot;: true,
-                        &quot;backSpeed&quot;: 30,
-                        &quot;backDelay&quot;: 2500
-                      }" />
+                      <span className="animatedText">
+                        <TypedText />
+                      </span>
                     </span>
                   </h1>
                   <p className="lead">We connect developers and organizations with each other for short to medium term projects.</p>
@@ -58,7 +57,7 @@ export default function HomePage() {
           <div className="col-lg-7 col-xl-6 d-none d-lg-block position-absolute top-0 end-0 pe-0" style={{marginTop: '6.75rem'}}>
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 1137.5 979.2">
               <path fill="#F9FBFF" d="M565.5,957.4c81.1-7.4,155.5-49.3,202.4-115.7C840,739.8,857,570,510.7,348.3C-35.5-1.5-4.2,340.3,2.7,389
-          c0.7,4.7,1.2,9.5,1.7,14.2l29.3,321c14,154.2,150.6,267.8,304.9,253.8L565.5,957.4z" />
+                c0.7,4.7,1.2,9.5,1.7,14.2l29.3,321c14,154.2,150.6,267.8,304.9,253.8L565.5,957.4z" />
               <defs>
                 <path id="mainHeroSVG1" d="M1137.5,0H450.4l-278,279.7C22.4,430.6,24.3,675,176.8,823.5l0,0C316.9,960,537.7,968.7,688.2,843.6l449.3-373.4V0z" />
               </defs>
@@ -66,7 +65,7 @@ export default function HomePage() {
                 <use xlinkHref="#mainHeroSVG1" />
               </clipPath>
               <g transform="matrix(1 0 0 1 0 0)" clipPath="url(#mainHeroSVG2)">
-                <Image width={750} height={750} src={heroImage} style={{transform: "matrix(1.4462 0 0 1.4448 52.8755 0)"}} alt='hero image' />
+                <Image src={heroImage} width={750} height={750} style={{ transform: "matrix(1.4462 0 0 1.4448 52.8755 0)" }} alt='hero image' />
               </g>
             </svg>
           </div>
