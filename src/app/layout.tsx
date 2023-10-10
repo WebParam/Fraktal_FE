f20013ca08dc3d856ca09ac47b57ea0e5dbb70ce
NjinuParam <njinu@webparam.org>
2023-10-10 10:32:03 +0300

    feat(up)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import "../assets/css/theme.min.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
