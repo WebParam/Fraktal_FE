37a348ea6c5cc412f1f3807483370c1e9f75d7ae
NjinuParam <njinu@webparam.org>
2023-10-10 12:16:39 +0300

    feat(font)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import "../assets/css/theme.min.css"
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins"
+          rel="stylesheet"
+        />
       <body className={inter.className}>{children}</body>
     </html>
   )
