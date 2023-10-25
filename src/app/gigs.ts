5c70844959ffee2ee27ccae9e1eed63fa013430e
Bradley Matjie <bradley@webparam.org>
2023-10-25 13:55:35 +0200

    can view single job post

---
+++ src/app/gigs.ts
@@ -0,0 +1,73 @@
+import mailchimp from "../assets/svg/brands/frontapp-icon.svg";
+import capsule from "../assets/svg/brands/capsule-icon.svg";
+import prosperus from "../assets/svg/brands/prosperops-icon.svg";
+import guideline from "../assets/svg/brands/guideline-icon.svg"; 
+
+export const gigs = [
+    {
+        id: 1,
+        companyname: 'J-T Consulting',
+        position: 'Senior Devops Engineer',
+        salary: 'R125k-R135k month',
+        location: 'Johannesburg',
+        jobType: 'Full Time',
+        img: mailchimp,
+        posted: 'today',
+        remote: 'remote'
+    },
+    {
+        id: 2,
+        companyname: 'Tuto',
+        position: 'Intermediate JS Front End Developer',
+        salary: 'R500 Hourly',
+        location: 'Pretoria',
+        jobType: 'Part Time',
+        img: capsule,
+        posted: 'today',
+        remote: 'remote'
+    },
+    {
+        id: 3,
+        companyname: 'Tuto',
+        position: 'Senior full-stack developer',
+        salary: 'R900 Hourly',
+        location: 'Pretoria',
+        jobType: 'Part Time',
+        img: capsule,
+        posted: '1 day ago',
+        remote: 'on-site'
+    },
+    {
+        id: 4,
+        companyname: 'Prosperops',
+        position: 'Senior Project Manager',
+        salary: 'R750 Hourly',
+        location: 'Cape Town',
+        jobType: 'Full Time',
+        img: prosperus,
+        posted: '1 day ago',
+        remote: 'on-site'
+    },
+    {
+        id: 5,
+        companyname: 'The App Lab',
+        position: 'Senior product manager',
+        salary: 'R75K per month',
+        location: 'Johannesburg',
+        jobType: 'Full Time',
+        img: capsule,
+        posted: '1 day ago',
+        remote: 'on-site'
+    },
+    {
+        id: 6,
+        companyname: 'Guideline',
+        position: 'iOS Engineer',
+        salary: 'R800 Hourly',
+        location: 'Johannesburg',
+        jobType: 'Part Time',
+        img: guideline,
+        posted: '1 day ago',
+        remote: 'remote'
+    }
+]
