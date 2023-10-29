1591d36448135e8da9b51dd17b589e7673397e94
NjinuParam <njinu@webparam.org>
2023-10-29 16:40:41 +0200

    feat(up)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8542899c4d6f65ae"; }
+/******/ 		__webpack_require__.h = function() { return "1c44cb9131063123"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
