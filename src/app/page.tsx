a6683e6829a60d993944c6c507d25f8c565594f9
atlegangkola <atlegang@webparam.org>
2024-01-22 11:18:22 +0200

    edited home page img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,13 @@ function HomePage() {
             {/* Card */}
             <div className="card card-sm h-100">
               <div className="p-2" >
-                <Image className="card-img" src={macbook}  alt="Image Description" />
+              <img
+                className="card-img"
+                src="https://res.cloudinary.com/domhrpxzy/image/upload/v1705914738/rsuia2h2tlt9utkt8ifr.png"
+                alt="Image Description"
+                width={300}
+                height={400}
+              />
               </div>
               <div className="card-body">
                 <h4 className="card-title">Softwear engineers:</h4>
