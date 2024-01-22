84a32dd52089a63b2da0ee13c42e79cf560eb04b
atlegangkola <atlegang@webparam.org>
2024-01-22 11:37:06 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['param-hr-resources.s3.af-south-1.amazonaws.com'],
+        domains: ['param-hr-resources.s3.af-south-1.amazonaws.com', 'res.cloudinary.com'],
       },
 }
+
+
+
 
 
 
