4e724dff9f3ddeb5f970652f68336f11f4c043fb
NjinuParam <njinu@webparam.org>
2024-01-24 10:16:05 +0200

    feat(up)

--- src/app/gigs.ts
+++ src/app/gigs.ts
@@ -4,80 +4,80 @@ import prosperus from "../assets/svg/brands/prosperops-icon.svg";
 import guideline from "../assets/svg/brands/guideline-icon.svg"; 
 
 export const gigs = [
-    // {
-    //     id: 1,
-    //     companyname: 'J-T Consulting',
-    //     position: 'Senior Devops Engineer',
-    //     salary: 'R125k-R135k month',
-    //     location: 'Johannesburg',
-    //     jobType: 'Full Time',
-    //     img: mailchimp,
-    //     posted: 'today',
-    //     remote: 'remote',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
-    // },
-    // {
-    //     id: 2,
-    //     companyname: 'Tuto',
-    //     position: 'Intermediate JS Front End Developer',
-    //     salary: 'R500 Hourly',
-    //     location: 'Pretoria',
-    //     jobType: 'Part Time',
-    //     img: capsule,
-    //     posted: 'today',
-    //     remote: 'remote',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
-    // },
-    // {
-    //     id: 3,
-    //     companyname: 'Tuto',
-    //     position: 'Senior full-stack developer',
-    //     salary: 'R900 Hourly',
-    //     location: 'Pretoria',
-    //     jobType: 'Part Time',
-    //     img: capsule,
-    //     posted: '1 day ago',
-    //     remote: 'on-site',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    {
+        id: 1,
+        companyname: 'J-T Consulting',
+        position: 'Senior Devops Engineer',
+        salary: 'R125k-R135k month',
+        location: 'Johannesburg',
+        jobType: 'Full Time',
+        img: mailchimp,
+        posted: 'today',
+        remote: 'remote',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    },
+    {
+        id: 2,
+        companyname: 'Tuto',
+        position: 'Intermediate JS Front End Developer',
+        salary: 'R500 Hourly',
+        location: 'Pretoria',
+        jobType: 'Part Time',
+        img: capsule,
+        posted: 'today',
+        remote: 'remote',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    },
+    {
+        id: 3,
+        companyname: 'Tuto',
+        position: 'Senior full-stack developer',
+        salary: 'R900 Hourly',
+        location: 'Pretoria',
+        jobType: 'Part Time',
+        img: capsule,
+        posted: '1 day ago',
+        remote: 'on-site',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
   
-    // },
-    // {
-    //     id: 4,
-    //     companyname: 'Prosperops',
-    //     position: 'Senior Project Manager',
-    //     salary: 'R750 Hourly',
-    //     location: 'Cape Town',
-    //     jobType: 'Full Time',
-    //     img: prosperus,
-    //     posted: '1 day ago',
-    //     remote: 'on-site',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    },
+    {
+        id: 4,
+        companyname: 'Prosperops',
+        position: 'Senior Project Manager',
+        salary: 'R750 Hourly',
+        location: 'Cape Town',
+        jobType: 'Full Time',
+        img: prosperus,
+        posted: '1 day ago',
+        remote: 'on-site',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
   
-    // },
-    // {
-    //     id: 5,
-    //     companyname: 'The App Lab',
-    //     position: 'Senior product manager',
-    //     salary: 'R75K per month',
-    //     location: 'Johannesburg',
-    //     jobType: 'Full Time',
-    //     img: capsule,
-    //     posted: '1 day ago',
-    //     remote: 'on-site',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    },
+    {
+        id: 5,
+        companyname: 'The App Lab',
+        position: 'Senior product manager',
+        salary: 'R75K per month',
+        location: 'Johannesburg',
+        jobType: 'Full Time',
+        img: capsule,
+        posted: '1 day ago',
+        remote: 'on-site',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
   
-    // },
-    // {
-    //     id: 6,
-    //     companyname: 'Guideline',
-    //     position: 'iOS Engineer',
-    //     salary: 'R800 Hourly',
-    //     location: 'Johannesburg',
-    //     jobType: 'Part Time',
-    //     img: guideline,
-    //     posted: '1 day ago',
-    //     remote: 'remote',
-    //     description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
+    },
+    {
+        id: 6,
+        companyname: 'Guideline',
+        position: 'iOS Engineer',
+        salary: 'R800 Hourly',
+        location: 'Johannesburg',
+        jobType: 'Part Time',
+        img: guideline,
+        posted: '1 day ago',
+        remote: 'remote',
+        description:"We are looking for a Senior DevOps Engineer to join our team. You will be responsible for helping to design, develop, and maintain our cloud infrastructure and services. You will be part of a small team of dedicated engineers who are passionate about delivering an exceptional user experience to our customers."
   
-    // }
+    }
 ]
